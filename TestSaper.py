a398aa470738a9951ad3bae5efe5b5b34c290c81
DESKTOP-JADVLLF\Mariusz <mariusz1614@poczta.onet.pl>
2019-01-22 09:39:37 +0100

    IO - 7 | New tests

--- TestSaper.py
+++ TestSaper.py
@@ -4,9 +4,9 @@ from tkinter import *
 from FileController import FileController
 import time
 from field import Field
-from SpecAsset import SpecAsset
-from FieldAsset import FieldAsset
-from Error import WrongDataException
+from asset.SpecAsset import SpecAsset
+from asset.FieldAsset import FieldAsset
+from exception.WrongDataException import WrongDataException
 
 class TestSaper(unittest.TestCase):
 
@@ -34,6 +34,71 @@ class TestSaper(unittest.TestCase):
         self.assertEqual(field.getTyp(), 'free')
         self.assertFalse(field.getBomb())
         self.assertTrue(field.getShown())
+
+    def test_loadSpecAsset(self):
+        stage= Tk()
+        specAsset = SpecAsset()
+
+        self.assertIsInstance(specAsset.getAssets()[1], PhotoImage)
+
+    def test_loadFieldAsset(self):
+        stage= Tk()
+        fieldAsset = FieldAsset()
+
+        self.assertIsInstance(fieldAsset.getAssets()[1], PhotoImage)
+
+    def test_gameResetWithWrongParameters(self):
+        stage = Tk()
+        game = Game(stage, 0, 0, 0)
+
+        with self.assertRaises(WrongDataException):
+            game.reset(16,16,14)
+
+    def test_gameResetWithWrongParameters2(self):
+        stage = Tk()
+        game = Game(stage, 0, 0, 0)
+
+        with self.assertRaises(ValueError):
+            game.reset('a','b','c')
+
+    def test_loadScore(self):
+        stage = Tk()
+        fileController = FileController(stage)
+
+        fileController.loadScore()
+        self.assertEqual(fileController.rekordy[0][0], 'Rekordy poziom Łatwy')
+
+    def test_fieldTypeAfterRightClickOnFreeField(self):
+        stage = Tk()
+        game = Game(stage, 4, 4, 0)
+        game._flags = 1
+        game.rightClick(0)
+
+        self.assertEqual(game._buttons[0].getTyp(), 'flag')
+
+    def test_fieldTypeAfterRightClickOnFlagedField(self):
+        stage = Tk()
+        game = Game(stage, 4, 4, 0)
+        game._buttons[0].setTyp("flag")
+        game.rightClick(0)
+
+        self.assertEqual(game._buttons[0].getTyp(), 'question')
+
+    def test_fieldTypeAfterRightClickOnQuestionedField(self):
+        stage = Tk()
+        game = Game(stage, 4, 4, 0)
+        game._buttons[0].setTyp("question")
+        game.rightClick(0)
+
+        self.assertEqual(game._buttons[0].getTyp(), 'free')
+
+    def test_lostStateAfterLeftClickOnBomb(self):
+        stage = Tk()
+        game = Game(stage, 4, 4, 0)
+        game._buttons[0].setBomb()
+        game.leftClick(0)
+
+        self.assertEqual(game._state, 'lost')
 
 if __name__ == '__main__':
     unittest.main()
